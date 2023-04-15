60ff03d66f6e261c73b7231626546571f70b76f6
anvirus <39328997+anvirus@users.noreply.github.com>
2023-04-15 10:07:51 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'http://img.233.com/union/union_pc/union-temphone/downapp-bg.png',
+      name: 'Jinma',
+      description: 'This is <a href="https://tsinma.com" class="text-blue-500" target="_blank" >Tsinma</a>',
     },
   }
 }
